9235d8eae45c8a3fb68ded2611223095b8c4d83b
nikitapermyakov <n.permikov@uzum.com>
2023-06-18 04:34:52 +0300

    Add simple text

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import profileImg from '../../asserts/profile.png';
+// import shapeOne from '../../asserts/shape-1.png';
+// import shapeTwo from '../../asserts/shape-2.png';
+import { FaTwitter, FaDribbble, FaBehance } from 'react-icons/fa';
+import './home.css';
+
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home' id='home'>
+        <div className='home__container container'>
+            <p className='home__subtitle text-cs'>Hello<span>My Name Is</span></p>
+            <p className='home__title text-cs'><span>ZOE</span>MILLER</p>
+            <p className='home__job'>
+                <span className='text-cs'>I Am</span> <b>Web DEveloper</b>
+            </p>
+        </div>
+    </section>
   )
 }
 

2e1109becd43898324847b8d7f4ef9cd610d3713
nikitapermyakov <n.permikov@uzum.com>
2023-06-18 11:35:23 +0300

    Add services section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,54 +9,56 @@ import './home.css';
 export const Home = () => {
   return (
     <section className='home' id='home'>
-        <div className='home__container container'>
-            <p className='home__subtitle text-cs'>Hello, <span>My Name Is</span></p>
-            <h1 className='home__title text-cs'><span>NASTYA</span> OMIK</h1>
-            <p className='home__job'>
-                <span className='text-cs'>I Am</span> <b>Lawyer</b>
-            </p>
-            <div className='home__img-wrapper'>
-                <div className='home__banner'>
-                    <img src={profileImg} alt='' className='home__profile'></img>
-                </div>
-                <p className='home__data home__data-one'>
-                    <span className='text-lg'>
-                        6 <b>+</b>
-                    </span>
-                    <span className='text-sm text-cs'>
-                        Years of <span>Experience</span>
-                    </span>
+        <div className='home__wrapper'>
+            <div className='home__container container'>
+                <p className='home__subtitle text-cs'>Hello, <span>My Name Is</span></p>
+                <h1 className='home__title text-cs'><span>NASTYA</span> OMIK</h1>
+                <p className='home__job'>
+                    <span className='text-cs'>I Am</span> <b>Lawyer</b>
                 </p>
-                <p className='home__data home__data-two'>
-                    <span className='text-lg'>
-                        330
-                    </span>
-                    <span className='text-sm text-cs'>
-                        Completed <span>Projects</span>
-                    </span>
-                </p>
+                <div className='home__img-wrapper'>
+                    <div className='home__banner'>
+                        <img src={profileImg} alt='' className='home__profile'></img>
+                    </div>
+                    <p className='home__data home__data-one'>
+                        <span className='text-lg'>
+                            6 <b>+</b>
+                        </span>
+                        <span className='text-sm text-cs'>
+                            Years of <span>Experience</span>
+                        </span>
+                    </p>
+                    <p className='home__data home__data-two'>
+                        <span className='text-lg'>
+                            330
+                        </span>
+                        <span className='text-sm text-cs'>
+                            Completed <span>Projects</span>
+                        </span>
+                    </p>
 
-                <img src={shapeOne} alt='' className='shape shape__1'></img>
-                <img src={shapeTwo} alt='' className='shape shape__2'></img>
-                <img src={shapeTwo} alt='' className='shape shape__3'></img>
-            </div>
-            <p className='home__text'>
-                From Portugal, Lisbon
-            </p>
-            <div className='home__socials'>
-                <a href='https://t.me/nikit34' className='home__social-link'>
-                    <FaTelegram />
-                </a>
-                <a href='https://github.com/nikit34' className='home__social-link'>
-                    <FaGithub />
-                </a>
-                <a href='https://www.linkedin.com/in/nikitapermikov/' className='home__social-link'>
-                    <FaLinkedin />
-                </a>
-            </div>
-            <div className='home__btns'>
-                <a href='https://github.com/' className='btn text-cs'>Download CV</a>
-                <a href='https://github.com/' className='hero__link text-cs'>My Skills</a>
+                    <img src={shapeOne} alt='' className='shape shape__1'></img>
+                    <img src={shapeTwo} alt='' className='shape shape__2'></img>
+                    <img src={shapeTwo} alt='' className='shape shape__3'></img>
+                </div>
+                <p className='home__text'>
+                    From Portugal, Lisbon
+                </p>
+                <div className='home__socials'>
+                    <a href='https://t.me/nikit34' className='home__social-link'>
+                        <FaTelegram />
+                    </a>
+                    <a href='https://github.com/nikit34' className='home__social-link'>
+                        <FaGithub />
+                    </a>
+                    <a href='https://www.linkedin.com/in/nikitapermikov/' className='home__social-link'>
+                        <FaLinkedin />
+                    </a>
+                </div>
+                <div className='home__btns'>
+                    <a href='https://github.com/' className='btn text-cs'>Download CV</a>
+                    <a href='https://github.com/' className='hero__link text-cs'>My Skills</a>
+                </div>
             </div>
         </div>
     </section>
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './services.css';
+
+
+const Services = () => {
+    return (
+        <section className="services section" id="services">
+            <h2 className="section__title text-cs">What I Do</h2>
+            <p className="section__subtitle">
+                My <span>Services</span>
+            </p>
+            <div className="services__container container"></div>
+        </section>
+    );
+};
+
+export default Services;

e72ebec59e318ed4c65d24ab604e11d26941a088
nikitapermyakov <n.permikov@uzum.com>
2023-06-18 13:03:45 +0300

    Add cards

---
+++ src/Data.jsx
@@ -0,0 +1,56 @@
+export const services = [
+    {
+        id: 1,
+        title: 'SDET (PYTHON, GOLANG), KASPERSKY',
+        name: 'KASPERSKYOS, APROTECH',
+        description: 'IoT in Industry, creating an environment for testing physical devices - gateways. Conducted performance testing'
+    },
+    {
+        id: 2,
+        title: 'PYTHON DEVELOPER, RAMBLER',
+        name: 'AFISHA',
+        description: 'Created unit tests, integration and mAPI tests. Created a pipeline for building iOS and Android applications and running tests with generating reports on the server'
+    },
+    {
+        id: 3,
+        title: 'JUNIOR PYTHON DEVELOPER, SBERBANK',
+        name: 'SBERUSER',
+        description: 'Built automation, set up a test environment, conducted unit, performance, bulk and functional testing, exploratory, tested api'
+    },
+    {
+        id: 4,
+        title: 'SDET (PYTHON, GOLANG), KASPERSKY',
+        name: 'KASPERSKYOS, APROTECH',
+        description: 'IoT in Industry, creating an environment for testing physical devices - gateways. Conducted performance testing'
+    },
+    {
+        id: 5,
+        title: 'PYTHON DEVELOPER, RAMBLER',
+        name: 'AFISHA',
+        description: 'Created unit tests, integration and mAPI tests. Created a pipeline for building iOS and Android applications and running tests with generating reports on the server'
+    },
+    {
+        id: 6,
+        title: 'JUNIOR PYTHON DEVELOPER, SBERBANK',
+        name: 'SBERUSER',
+        description: 'Built automation, set up a test environment, conducted unit, performance, bulk and functional testing, exploratory, tested api'
+    },
+    {
+        id: 7,
+        title: 'SDET (PYTHON, GOLANG), KASPERSKY',
+        name: 'KASPERSKYOS, APROTECH',
+        description: 'IoT in Industry, creating an environment for testing physical devices - gateways. Conducted performance testing'
+    },
+    {
+        id: 8,
+        title: 'PYTHON DEVELOPER, RAMBLER',
+        name: 'AFISHA',
+        description: 'Created unit tests, integration and mAPI tests. Created a pipeline for building iOS and Android applications and running tests with generating reports on the server'
+    },
+    {
+        id: 9,
+        title: 'JUNIOR PYTHON DEVELOPER, SBERBANK',
+        name: 'SBERUSER',
+        description: 'Built automation, set up a test environment, conducted unit, performance, bulk and functional testing, exploratory, tested api'
+    }
+]

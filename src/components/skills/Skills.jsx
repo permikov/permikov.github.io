24499166804485cd100e3d52d4aca8022d91576a
nikitapermyakov <n.permikov@uzum.com>
2023-06-18 20:39:39 +0300

    Add perceptile

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -22,7 +22,7 @@ const Skills = () => {
                             </div>
                             <p className="skills__description">{description}</p>
                             <div className="skills__bar">
-                                <span className="skills__percentage">
+                                <span className="skills__percentage" style={{ width: `${percentage}%` }}>
                                     <span></span>
                                 </span>
                             </div>

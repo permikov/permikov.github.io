d8be870ef9a1ace44f134fa6e29e72fe6aa74833
nikitapermyakov <n.permikov@uzum.com>
2023-06-23 21:33:00 +0300

    Add data for project section

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,3 +1,9 @@
+import Project1 from './asserts/project_graph.png';
+import Project2 from './asserts/project_light.png';
+import Project3 from './asserts/project_multiplayer.png';
+import Project4 from './asserts/project_rpg.png';
+
+
 export const services = [
     {
         id: 1,
@@ -78,6 +84,37 @@ export const skills = [
         id: 4,
         name: 'bash',
         percentage: 90,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper diam urna, sed sodales felis facilisis ut. Duis libero dolor, pretium vel sapien sed, tempus interdum mi'
+    }
+]
+
+export const projects = [
+    {
+        id: 1,
+        img: Project1,
+        category: 'Tools',
+        title: 'Tools for theory graph',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper diam urna, sed sodales felis facilisis ut. Duis libero dolor, pretium vel sapien sed, tempus interdum mi'
+    },
+    {
+        id: 2,
+        img: Project2,
+        category: 'IoT',
+        title: 'IoT light',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper diam urna, sed sodales felis facilisis ut. Duis libero dolor, pretium vel sapien sed, tempus interdum mi'
+    },
+    {
+        id: 3,
+        img: Project3,
+        category: 'Game',
+        title: 'Multiplayr',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper diam urna, sed sodales felis facilisis ut. Duis libero dolor, pretium vel sapien sed, tempus interdum mi'
+    },
+    {
+        id: 4,
+        img: Project4,
+        category: 'Game',
+        title: '2D Shooter',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper diam urna, sed sodales felis facilisis ut. Duis libero dolor, pretium vel sapien sed, tempus interdum mi'
     }
 ]

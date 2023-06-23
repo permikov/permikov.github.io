0d80ad9a2cd51bfa9b296101a87c73a861f12b1d
nikitapermyakov <n.permikov@uzum.com>
2023-06-24 00:20:57 +0300

    Add new style rules

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/home/Home';
 import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
+import Portfolio from './components/portfolio/Portfolio';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <Services />
       <Skills />
+      <Portfolio />
     </main>
   );
 }

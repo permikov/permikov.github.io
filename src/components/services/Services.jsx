fe87fc85bbe2d4402fee54bbc5691d75c9ba919f
nikitapermyakov <n.permikov@uzum.com>
2023-06-24 00:22:01 +0300

    Add index

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -38,7 +38,7 @@ const Services = () => {
                 className="services__container container mySwiper">
                 {services.map(({name, title, description}, index) => {
                     return (
-                        <SwiperSlide className="services__item card card-one">
+                        <SwiperSlide className="services__item card card-one" key={index}>
                             <span className="services__subtitle text-cs">{name}</span>
                             <h3 className="services__title">{title}</h3>
                             <p className="services__description">{description}</p>

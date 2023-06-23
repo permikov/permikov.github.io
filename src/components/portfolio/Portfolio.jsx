07b152cbc55ede2438fad83210f598a681876bf1
nikitapermyakov <n.permikov@uzum.com>
2023-06-24 00:22:18 +0300

    Add body profile

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import List from './List';
+import Items from './Items';
+import { projects } from '../../Data';
+import "./portfolio.css";
+
+
+const allNavList = projects.map((project) => project.category);
 
 const Portfolio = () => {
+    const [projectItems, setMenuItems] = useState(projects);
   return (
-    <div>Portfolio</div>
-  )
-}
+    <section className='portfolio section' id='work'>
+        <h2 className="section__title text-cs">Portfolio</h2>
+        <p className="section__subtitle">
+            My <span>Cases</span>
+        </p>
+        <List />
+        <div className="portfolio__container container grid">
+            <Items projectItems={projectItems} />
+        </div>
+    </section>
+  );
+};
 
 export default Portfolio;

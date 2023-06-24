4588b6a4f1bb2a998235964a3b5208784822661b
nikitapermyakov <n.permikov@uzum.com>
2023-06-24 16:33:38 +0300

    Add resume

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/home/Home';
 import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
 import Portfolio from './components/portfolio/Portfolio';
+import Resume from './components/resume/Resume';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Services />
       <Skills />
       <Portfolio />
+      <Resume />
     </main>
   );
 }

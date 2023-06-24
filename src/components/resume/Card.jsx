6454efef4be8e29eada374d0541e29852528f30a
nikitapermyakov <n.permikov@uzum.com>
2023-06-24 23:38:12 +0300

    Add change to plus - minus

--- src/components/resume/Card.jsx
+++ src/components/resume/Card.jsx
@@ -1,13 +1,15 @@
-import React from "react"
+import React, { useState } from "react";
+
 
 const Card = (props) => {
+  const [showInfo, setShowInfo] = useState(false);
   return (
     <div className="resume__item">
-        <div className="resume__header">
+        <div className="resume__header" onClick={() => setShowInfo(!showInfo)}>
             <h3 className="resume__subtitle">{props.title}</h3>
-            <span className="resume__icon">-</span>
+            <span className="resume__icon">{showInfo ? '-' : '+'}</span>
         </div>
-        <div className="resume__content">
+        <div className={`${showInfo ? 'show-content' : ''} resume__content`}>
             <div className="resume__date-title">
                 <h3 className="resume__title">{props.subtitle}</h3>
                 <span className="resume__date text-cs">{props.date}</span>
@@ -15,7 +17,7 @@ const Card = (props) => {
             <p className="resume__description">{props.description}</p>
         </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;

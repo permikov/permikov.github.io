05970aa70370bf8e579471ee34b6efe511a025b8
nikitapermyakov <n.permikov@uzum.com>
2023-06-25 11:29:13 +0300

    Add block with article

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,8 +1,57 @@
 import React from 'react';
+import { FaArrowRight } from 'react-icons/fa';
+import Blog1 from '../../asserts/blog1.jpg';
+import Blog2 from '../../asserts/blog2.jpg';
+import Blog3 from '../../asserts/blog3.jpg';
+import './blog.css';
+
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section className="blog section" id="blog">
+        <h2 className="section__title text-cs">Latest Blog</h2>
+        <p className="section__subtitle">
+            My <span>Articles and Advice</span>
+        </p>
+        <div className="blog__container container grid">
+            <div className="blog__item card card-two">
+                <span className="blog__date text-cs">OCTOBER 31, 2022</span>
+                <h3 className="blog__title">The Main Thing For The Designer</h3>
+                <p className="blog__description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper diam urna, sed sodales felis facilisis ut. Duis libero dolor, pretium vel sapien sed, tempus interdum mi
+                </p>
+                <a href="" className="link">
+                    Read more
+                    <FaArrowRight className="link__icon"></FaArrowRight>
+                </a>
+                <img src={Blog1} alt="" className="blog__img" />
+            </div>
+            <div className="blog__item card card-two">
+                <span className="blog__date text-cs">OCTOBER 31, 2022</span>
+                <h3 className="blog__title">Follow Your Own Design Process</h3>
+                <p className="blog__description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper diam urna, sed sodales felis facilisis ut. Duis libero dolor, pretium vel sapien sed, tempus interdum mi
+                </p>
+                <a href="" className="link">
+                    Read more
+                    <FaArrowRight className="link__icon"></FaArrowRight>
+                </a>
+                <img src={Blog2} alt="" className="blog__img" />
+            </div>
+            <div className="blog__item card card-two">
+                <span className="blog__date text-cs">NOVEMBER 28, 2022</span>
+                <h3 className="blog__title">Usability Secrets to Create Better Interfaces</h3>
+                <p className="blog__description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper diam urna, sed sodales felis facilisis ut. Duis libero dolor, pretium vel sapien sed, tempus interdum mi
+                </p>
+                <a href="" className="link">
+                    Read more
+                    <FaArrowRight className="link__icon"></FaArrowRight>
+                </a>
+                <img src={Blog3} alt="" className="blog__img" />
+            </div>
+        </div>
+    </section>
   );
 };
 
